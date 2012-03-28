9437b2b9c9efbe2d19e1cc1a466d8d926c286c6b
David Egolf <pheasants24@gmail.com>
2012-03-28 10:43:10 -0400

    initial commit

---
+++ csc237_project2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  csc237_project2
+//
+//  Created by David Egolf on 3/28/12.
+//  Copyright (c) 2012 Kutztown University. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
