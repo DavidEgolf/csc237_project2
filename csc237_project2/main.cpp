4047c4523cd7508a8c24e4a9732ed0d04e624cde
David Egolf <pheasants24@gmail.com>
2012-03-28 22:17:45 -0400

    bunch of new stuff!

--- csc237_project2/main.cpp
+++ csc237_project2/main.cpp
@@ -3,16 +3,99 @@
 //  csc237_project2
 //
 //  Created by David Egolf on 3/28/12.
-//  Copyright (c) 2012 Kutztown University. All rights reserved.
+//  Copyright (c) 2012 David Egolf. All rights reserved.
 //
+#define TRUE true
+#define FALSE false
 
 #include <iostream>
+#include <stack>
+
+using namespace std;
+
+void getExpression(string & expression);
+bool evaluateParenthesis(string);
+void printPostfix(string);
+bool isNumeric(char);
+bool isOperator(char);
+
 
 int main (int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "0.0.1\n";
+    
+    string expressionString;
+    stack<char> operators;
+    stack<double> numbers;
+    
+    getExpression(expressionString);
+    
+    if(evaluateParenthesis(expressionString))
+        cout << "Parenthesis matched.\n";
+    else
+        cout << "Parenthesis mismatched.\n";
+    
     return 0;
 }
+
+void getExpression(string & expression)
+{
+    getline(cin, expression);
+}
+
+bool evaluateParenthesis(string inputString)
+{
+    stack<char> parenthesis;
+    
+    for(int i = 0; i < inputString.size(); i++)
+    {
+        if(inputString[i] == '(')
+            parenthesis.push(inputString[i]);
+    }
+    
+    for(int i = 0; i < inputString.size(); i++)
+    {
+        if (inputString[i] == ')') 
+        {
+            parenthesis.pop();
+        }
+    }
+    
+    if (parenthesis.size())
+        return FALSE;
+    else
+        return TRUE;
+}
+
+void printPostfix(string expression)
+{
+    stack <char> operators;
+}
+
+bool isNumeric(char charToCompair)
+{
+    for(int i = 48; i < 58; i++)
+    {
+        if((int)charToCompair == i) 
+        {
+            return TRUE;
+        }
+    }
+    return FALSE;
+}
+
+bool isOperator(char charToCompair)
+{
+    switch (charToCompair) 
+    {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            return TRUE;
+        default:
+            return FALSE;
+    }
+}
+
 
